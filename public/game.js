ee281978fdd1b937670377b9c8231f5fa899a2b3
Elliot Cheung <elliotccheung@gmail.com>
2016-11-28 14:11:27 +0000

    Added singular player which tracks mouse's x pos

--- public/game.js
+++ public/game.js
@@ -4,6 +4,9 @@ let gameSettings = {
 };
 
 let game = new Phaser.Game(gameSettings.width, gameSettings.height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+let tracking = false;
+let players;
+let activePlayer;
 
 function preload() {
   game.load.image("background", "assets/background.png");
@@ -13,8 +16,24 @@ function preload() {
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.add.sprite(0, 0, "background");
+
+  players = game.add.group();
+  let p1 = players.create(game.world.centerX, game.world.centerY, "player");
+  players.enableBody = true;
+  p1.inputEnabled = true;
+  p1.events.onInputDown.add(setActivePlayer, this);
+
 }
 
 function update() {
+  if(game.input.mousePointer.isUp) {
+    activePlayer = undefined;
+  }
+  if(activePlayer) {
+    activePlayer.x = game.input.x;
+  }
+}
 
+function setActivePlayer(player) {
+  activePlayer = player;
 }
